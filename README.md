766aa2614af9ad4d0dc76f9f261e549bed96bd5b
Zehui-stjude <zyu@stjude.org>
2026-02-26 15:14:48 -0600

    update By Zehui312

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Bacterial_SV_pipeline
+Bacterial_SV_pipeline is a workflow for analyzing bacterial structure variation based on reads-level.
+
+# 1 Installing Conda 
+We recommend installing miniconda to creat a enviroment for this pipeline.
+
+```
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
+conda install mamba==2.0.5
+```
+
+# 2 Creat enviroment for SV_pipeline
+
+```
+mamba env create -f sv_pipeline_env.yml
+```
+
+# 3 Running test 
+
+```
+sh sniffles2_analysis.sh \
+-l /research/groups/ma1grp/home/zyu/work_2026/Pipeline_data/SV_pipeline_data/sample_3_nanopore.fastq.gz \
+-f /research/groups/ma1grp/home/zyu/work_2026/Pipeline_data/SV_pipeline_data/sample_1.fna \
+-o /research/groups/ma1grp/home/zyu/work_2026/SV_2_Feb/SV_pipeline/output \
+-s F2 \
+-q 15 \
+-p /research/groups/ma1grp/home/zyu/work_2026/SV_2_Feb/SV_pipeline/script/stat_breakpoints.py
+```
--- sniffles2_analysis.sh
+++ sniffles2_analysis.sh
@@ -3,7 +3,7 @@
 # output_path=/research/groups/ma1grp/home/zyu/work_2026/SV_2_Feb/SV_pipeline/output
 # sample_name="sample_3"
 # long_quality=15
-# stat_frequency_script=/research/groups/ma1grp/home/zyu/work_2026/SV_2_Feb/SV_pipeline/script/stat_breakpoints.py
+# stat_frequency_script=/research/groups/ma1grp/home/zyu/work_2026/SV_2_Feb/SV_pipeline/Bacterial_SV_pipeline/stat_breakpoints.py
 
 
 # Parse command line arguments
@@ -35,8 +35,8 @@ fi
 #=================================================================
 #+++++++++++++++++++++++Step 1 mapping Long ++++++++++++++++++++++
 #=================================================================
-mkdir -p ${output_path}/1_mapping_Long
-cd ${output_path}/1_mapping_Long
+mkdir -p ${output_path_dir}/1_mapping_Long
+cd ${output_path_dir}/1_mapping_Long
 
 ln -s ${long_read_path} .
 
@@ -52,40 +52,39 @@ samtools index ${sample_name}.bam
 #=================================================================
 #+++++++++++++++++++++++Step 2 SV calling ++++++++++++++++++++++++
 #=================================================================
-mkdir -p ${output_path}/2_SV_calling
-cd ${output_path}/2_SV_calling
+mkdir -p ${output_path_dir}/2_SV_calling
+cd ${output_path_dir}/2_SV_calling
 
-sniffles --input ${output_path}/1_mapping_Long/${sample_name}.bam --vcf ${sample_name}_SV_all.vcf --reference ${fna_ref_path} --all-contigs
-sniffles --input ${output_path}/1_mapping_Long/${sample_name}.bam --vcf ${sample_name}_SV_chromosome.vcf --reference ${fna_ref_path} 
+
+sniffles --input ${output_path_dir}/1_mapping_Long/${sample_name}.bam --vcf ${sample_name}_SV_chromosome.vcf --reference ${fna_ref_path} 
 
 #=================================================================
 #+++++++++++++++++++++++Step 3 Chimeric reads ++++++++++++++++++++
 #=================================================================
-mkdir -p ${output_path}/3_Chimeric_reads
-cd ${output_path}/3_Chimeric_reads
+mkdir -p ${output_path_dir}/3_Chimeric_reads
+cd ${output_path_dir}/3_Chimeric_reads
 
 
-samtools view -h ${output_path}/1_mapping_Long/${sample_name}.bam | awk 'BEGIN {OFS="\t"} /^@/ || $0 ~ /SA:Z:/ {print $0}' > ${sample_name}_chimeric.sam
+samtools view -h ${output_path_dir}/1_mapping_Long/${sample_name}.bam | awk 'BEGIN {OFS="\t"} /^@/ || $0 ~ /SA:Z:/ {print $0}' > ${sample_name}_chimeric.sam
 samtools view -bS ${sample_name}_chimeric.sam > ${sample_name}_chimeric.bam
 samtools index ${sample_name}_chimeric.bam
 
 #=================================================================
 #+++++++++++++++++++++++Step 4 SV calling by chimeric reads ++++++
 #=================================================================
-mkdir -p ${output_path}/4_SV_calling_chimeric
-cd ${output_path}/4_SV_calling_chimeric
-sniffles --input ${output_path}/3_Chimeric_reads/${sample_name}_chimeric.bam --vcf ${sample_name}_SV_chimeric_all.vcf --reference ${fna_ref_path} --all-contigs
-sniffles --input ${output_path}/3_Chimeric_reads/${sample_name}_chimeric.bam --vcf ${sample_name}_SV_chimeric_chromosome.vcf --reference ${fna_ref_path}
+mkdir -p ${output_path_dir}/4_SV_calling_chimeric
+cd ${output_path_dir}/4_SV_calling_chimeric
+sniffles --input ${output_path_dir}/3_Chimeric_reads/${sample_name}_chimeric.bam --vcf ${sample_name}_SV_chimeric_chromosome.vcf --reference ${fna_ref_path}
 
 #=================================================================
 #+++++++++++++++++++++++Step 5 stat frequency ++++++++++++++++++++
 #=================================================================
-mkdir -p ${output_path}/5_SV_frequency
-cd ${output_path}/5_SV_frequency
+mkdir -p ${output_path_dir}/5_SV_frequency
+cd ${output_path_dir}/5_SV_frequency
 
 
-cat ${output_path}/4_SV_calling_chimeric/${sample_name}_SV_chimeric_chromosome.vcf |grep -v "^#" |grep "PRECISE" | awk '{print $1"\t"$2"\t"$3"\t"$5"\t"$8}' | sed 's/PRECISE.*END=//g' | sed 's/;.*//g' > ${sample_name}_SV_chromosome.bed
-
+cat ${output_path_dir}/4_SV_calling_chimeric/${sample_name}_SV_chimeric_chromosome.vcf |awk '{if ($2 > 100) print $0}' |grep "PRECISE" | awk '{print $1"\t"$2"\t"$3"\t"$5"\t"$8}' | sed 's/PRECISE.*END=//g' | sed 's/;.*//g' > ${sample_name}_SV_chromosome.bed
+cp ${stat_frequency_script} .
 count=1
 cat ${sample_name}_SV_chromosome.bed | while read line; do
     contig_a=$(echo $line | cut -f1 -d' ')
@@ -101,7 +100,7 @@ cat ${sample_name}_SV_chromosome.bed | while read line; do
         contig_b=${contig_a}
         breakpoint_b=${fifth_field}
     fi
-    python stat_breakpoints.py --bam ${output_path}/1_mapping_Long/${sample_name}.bam --contig-a ${contig_a} --breakpoint-a ${breakpoint_a} --contig-b ${contig_b} --breakpoint-b ${breakpoint_b} --slop 5 --span-cutoff 15 --sample-name ${sample_name}-${count}-${sv_type}
+    python stat_breakpoints.py --bam ${output_path_dir}/1_mapping_Long/${sample_name}.bam --contig-a ${contig_a} --breakpoint-a ${breakpoint_a} --contig-b ${contig_b} --breakpoint-b ${breakpoint_b} --slop 5 --span-cutoff 15 --sample-name ${sample_name}-${count}-${sv_type}
     count=$((count + 1))
 done 
 
@@ -109,8 +108,3 @@ done
 
 
 
-python stat_breakpoints.py \
-  --bam ${sample_name}.bam --contig-a contig_1 --breakpoint-a 573076 \
-  --contig-b contig_3 --breakpoint-b 103809 \
-  --slop 5 --span-cutoff 15 \
-  --sample-name test
